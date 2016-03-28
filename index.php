8b09ec3b19b4aa3d45ed1230c7d31f4373b994ec
adri326 <adrien.burgun@orange.fr>
2016-03-28 15:54:05 +0200

    There is the basic file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php $pageName = "Newawe"; include('/start.php'); ?>
+      <ul class="TOP" id='indul'>
+        <li><img src='PNG.png' width="50px"></li>
+        <li class='hs'><a href="login.html">Login</a></li>
+        <li class='hs'><a href="http://newawe.gwiddle.co.uk/Fourm/">Forums</a></li>
+        <li class='hs'><a href="contact.html">Contact</a></li>
+        <li class='hs'><a href="#">Create</a></li>
+      </ul>
+      <!--<div class="w3-container" style="background-color:#00A3C6;">
+        <h1>Newawe</h1>
+      </div>-->
+      <div class="w3-container">
+        <p>Share Your Code. With the World.</p>
+        <img src="http://bjskistad.github.io/newawe/images/Welcome.jpg" width="50%">
+      </div>
+<?php include('/end.php'); ?>
