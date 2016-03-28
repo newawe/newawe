a3cccb19ac42fa8dc41e1d1c7ec39a66f5b81db2
adri326 <adrien.burgun@orange.fr>
2016-03-28 15:59:50 +0200

    That's the new version of the contact file.
    Please use overtime you create a new page the include('start.php');

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php $pagen = "Newawe - contact"; include('start.php');?>
+      <ul class="TOP" id='conul'>
+        <li><img src='https://cdn2.scratch.mit.edu/get_image/user/8706392_55x55.png?v=1455575316.54'></li>
+        <li class='hs'><a href="index.html">Home</a></li>
+        <li class='hs'><a href="login.html">Login</a></li>
+        <li class='hs'><a href="http://newawe.gwiddle.co.uk/Fourm/">Forums</a></li>
+        <li class='hs'><a href="#">Create</a></li>
+      </ul>
+<?php include('end.php'); ?>
