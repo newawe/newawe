a41c5154c276dacfd6254ca8b9b68328ec42b2aa
adri326 <adrien.burgun@orange.fr>
2016-03-28 15:49:30 +0200

    Create start.php

---
+++ start.php
@@ -0,0 +1,24 @@
+<?php /*
+
+Start content of every pages, use include('/start.php'); or include_once('/start.php');
+
+*/?>
+<html>
+  <head>
+    <title>
+      <?php echo $pageName; ?>
+    </title>
+    <link rel="stylesheet" href="/main.css" />
+  </head>
+  <body>
+    <header class="title">
+      <h1>
+        <?php echo $pageName; ?>
+      </h1>
+    </header>
+    <section class="content">
+      <?php /*
+
+EOF
+
+*/?>
