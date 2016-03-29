8ca6ea7267fd046441694d0c95db0f21c87cbb61
robinp7720 <robin@decker.cx>
2016-03-30 00:07:29 +0200

    Finished helper class to get user credential info

--- classes/helpers/credentials.php
+++ classes/helpers/credentials.php
@@ -10,7 +10,78 @@ class credentials
 {
     public $mysqli;
 
-    public function idExist($id,$mysql = null) {
-        ($mysql===null) ?: $mysqli = $this->mysqli;
+    public function idExist($id, $mysql = null)
+    {
+        ($mysql === null) ?: $mysql = $this->mysqli;
+        /* create a prepared statement */
+        $stmt = $mysql->stmt_init();
+        if ($stmt->prepare("SELECT count(*) FROM nw_users WHERE row_id=? limit 1")) {
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $count = 0;
+            $stmt->bind_result($count);
+            $stmt->fetch();
+            $stmt->close();
+            return $count > 0;
+        }
+        return false;
+    }
+
+    public function userExist($username, $mysql = null)
+    {
+        ($mysql === null) ?: $mysql = $this->mysqli;
+        /* create a prepared statement */
+        $stmt = $mysql->stmt_init();
+        if ($stmt->prepare("SELECT count(*) FROM nw_users WHERE username=? limit 1")) {
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $count = 0;
+            $stmt->bind_result($count);
+            $stmt->fetch();
+            $stmt->close();
+            return $count > 0;
+        }
+        return false;
+    }
+
+    public function getPasswordHash($id, $mysql = null)
+    {
+        ($mysql === null) ?: $mysql = $this->mysqli;
+        if (is_int($id)){
+            return $this->getPasswordHashId($id, $mysql);
+        }
+        return $this->getPasswordHashUser($id, $mysql);
+    }
+
+    private function getPasswordHashId($id, $mysql)
+    {
+        /* create a prepared statement */
+        $stmt = $mysql->stmt_init();
+        if ($stmt->prepare("SELECT password FROM nw_users WHERE row_id=? limit 1")) {
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $hash = null;
+            $stmt->bind_result($hash);
+            $stmt->fetch();
+            $stmt->close();
+            return $hash;
+        }
+        return false;
+    }
+
+    private function getPasswordHashUser($username, $mysql)
+    {
+        /* create a prepared statement */
+        $stmt = $mysql->stmt_init();
+        if ($stmt->prepare("SELECT password FROM nw_users WHERE username=? limit 1")) {
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $hash = null;
+            $stmt->bind_result($hash);
+            $stmt->fetch();
+            $stmt->close();
+            return $hash;
+        }
+        return false;
     }
 }
