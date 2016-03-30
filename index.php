df484e9731c9ff6e8c523b9418c12ecc8812d43e
Radical-from-Scratch <radicalfromscratch@gmail.com>
2016-03-29 19:43:06 -0700

    Disallowing those without javascript

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . "/classes/Newawe.php";
 require __DIR__ . "/classes/view.php";
+echo ('<noscript><meta http-equiv="REFRESH" content="0;url=http://enable-javascript.com"></noscript>');
 
 $newawe = new Newawe();
 $newawe->setPage(isset($_GET['p']) ? $_GET['p'] : "index");
