3058213396ddc5e8685e980a07c26acceb91a622
Radical-from-Scratch <radicalfromscratch@gmail.com>
2016-03-30 08:28:32 -0700

    moving robin's database set to the top---i'm gonna use that one

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
  define('SRV_ROOT', dirname(__FILE__));
   require __DIR__ . "/classes/Newawe.php";
   require __DIR__ . "/classes/view.php";
+  
+   $mysqli = new mysqli($DbConf['host'], $DbConf['user'], $DbConf['password'], $DbConf['database']);
+  
  ini_set('magic_quotes_runtime', 0);
  ini_set('session.save_path', SRV_ROOT . '/data/sessions.sqlite');
  ini_set('session.save_handler', 'sqlite');
@@ -51,9 +54,42 @@ require __DIR__ . "/classes/helpers/hash.php";
  require __DIR__ . "/classes/helpers/credentials.php";
  
  $DbConf = require __DIR__."/configs/mysql.php";
+  // dump the buffer into a string for manipulation
+    $contents = ob_get_contents();
+    ob_end_clean();
+
+    // set the appropriate content type
+    if ($content_type) {
+        header('Content-type: ' . $content_type);
+    } else {
+        header('Content-type: text/html; charset=utf-8');
+    }
+
+    // modify the page title according to the variable, if present
+    if ($page_info['header']) {
+        if(!isset($page_title))
+            $page_title = 'Mod Share';
+        $contents = str_replace('<$page_title/>', $page_title, $contents);
+    }
+
+    // output the page
+    echo $contents;
+} else {
+    // if not found, echo a 404 page
+    if ($permission_error) {
+        header('HTTP/1.1 403 Forbidden');
+        include SRV_ROOT . '/errorpages/permission_error.php';
+    } else {
+        header('HTTP/1.1 404 Not found');
+        include SRV_ROOT . '/errorpages/404.php';
+    }
+}
+
+$_SESSION['lastvisit'] = time();
+$db->close();
  
  // Setup mysql connection
- $mysqli = new mysqli($DbConf['host'], $DbConf['user'], $DbConf['password'], $DbConf['database']);
+
  
  $newawe = new Newawe($mysqli);
   $newawe>setPage(isset($_GET['p']) ? $_GET['p'] : "home");
