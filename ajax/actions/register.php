1b1a23b21be97d3fcc13a1dc05e9c0834a036ae7
robinp7720 <robin@decker.cx>
2016-03-30 16:55:01 +0200

    fixed register action

--- ajax/actions/register.php
+++ ajax/actions/register.php
@@ -8,12 +8,13 @@ $DbConf = require __DIR__ . "/../../configs/mysql.php";
 // Setup mysql connection
 $mysqli = new mysqli($DbConf['host'], $DbConf['user'], $DbConf['password'], $DbConf['database']);
 
-if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['country']) && is_int($_POST['country'])) {
-    if (credentials::userExist($_POST['username'],$mysqli)) {
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['country'])) {
+    if (!credentials::userExist($_POST['username'],$mysqli)) {
         /* create a prepared statement */
         $stmt = $mysqli->stmt_init();
         if ($stmt->prepare("INSERT INTO nw_users(username, password, email, country) values (?,?,?,?)")) {
-            $stmt->bind_param("ssss", $_POST['username'], PasswordHash::hash($_POST['password']), $_POST['email'], $_POST['country']);
+            $hash = PasswordHash::hash($_POST['password']);
+            $stmt->bind_param("ssss", $_POST['username'], $hash, $_POST['email'], $_POST['country']);
             $stmt->execute();
             $stmt->close();
             $data = ["success"];
