468b6f7c490e3612cbe56ee56d315f6c5e90d443
HittmanA <hittmana@comcast.net>
2016-06-22 09:23:46 -0500

    Fixed inline control statements READ DESCRIPTION
    Guys remember to put braces after if and else statements otherwise the may bug out.
    example:
    bad:
        if ('1' = '1')
            echo 'hi';
    good:
        if ('1' = '1') {
            echo 'hi';
    }

--- ajax/index.php
+++ ajax/index.php
@@ -4,7 +4,10 @@
 // Set content type to json
 header('Content-Type: application/json');
 
-if (isset($data))
+if (isset($data)) {
     echo json_encode($data);
+}
 else
+{
     echo json_encode(["error"=>["code"=>1, "message"=>"Function not found"]]);
+}
