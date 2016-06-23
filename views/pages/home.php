ff992431ef925bbeefe05ec2c50d767fa64fe007
Ron Friedman <ron852719@icloud.com>
2016-06-23 15:28:36 -0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,5 +1,5 @@
-<div class="info text-center">
+<div class="info text-center" style="overflow:hidden;height:100%;width:100%">
     <span>{{ loggedin }}</span>
     <span>Share Your Code. With the World.</span>
-    <object data="http://blog.newawe.com/toolserver/player/" width="100%" height="100%" style="overflow:hidden;height:100%;width:100%"></object>
+    <object data="http://blog.newawe.com/toolserver/player/" width="100%" height="100%"></object>
 </div>
