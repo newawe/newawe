7b0dcf6a5a8c69795f9c27c2141b56645599641e
Hampton <herohamp@users.noreply.github.com>
2016-06-23 22:34:16 -0400

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,13 +1,24 @@
 <div class="info text-center" style="overflow:hidden;height:100%;width:100%">
     <span>{{ loggedin }}</span>
     <span>Share Your Code. With the World.</span>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<p id="Show"></p>
+    
+<div id="Show"></div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script>
-$(document).ready(function(){
-        $.get("http://blog.newawe.com/toolserver/player/", function(data, status){
-            document.getElementById("Show").innerHTML = data;
-        });
-});
+ var xhttp;
+  if (window.XMLHttpRequest) {
+    // code for modern browsers
+    xhttp = new XMLHttpRequest();
+    } else {
+    // code for IE6, IE5
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("Show").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "http://blog.newawe.com/toolserver/player/", true);
+  xhttp.send();
 </script>
 </div>
