5a89fe78901a79ab0aecfda79e2b2adf2c39b480
Hampton <herohamp@users.noreply.github.com>
2016-06-24 19:19:27 -0400

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -18,7 +18,7 @@
       document.getElementById("Show").innerHTML = xhttp.responseText;
     }
   };
-  xhttp.open("GET", "http://blog.newawe.com/toolserver/player/", true);
+  xhttp.open("GET", "http://toolserver.newawe.com/player", true);
   xhttp.send();
 </script>
 </div>
