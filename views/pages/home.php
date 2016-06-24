df7e9997fa385fa0c04300fa5c2000a322f4566b
Hampton <herohamp@users.noreply.github.com>
2016-06-23 22:25:34 -0400

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,5 +1,13 @@
 <div class="info text-center" style="overflow:hidden;height:100%;width:100%">
     <span>{{ loggedin }}</span>
     <span>Share Your Code. With the World.</span>
-    <object data="http://blog.newawe.com/toolserver/player/" width="100%" height="100%" scrolling='yes'></object>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<p id="Show"></p>
+<script>
+$(document).ready(function(){
+        $.get("http://blog.newawe.com/toolserver/player/", function(data, status){
+            document.getElementById("Show").innerHTML = data;
+        });
+});
+</script>
 </div>
