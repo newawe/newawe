19753de63a88d15a3dd618feb0395ddf07d7d4d8
Jonathan50 <Jonathan50@users.noreply.github.com>
2016-10-13 17:44:32 +1300

    Seperate sign up and login javascript

--- assets/javascripts/register.js
+++ assets/javascripts/register.js
@@ -2,7 +2,7 @@
  * Created by robin on 3/29/16.
  */
 
-ajax("ajax/actions/countries.php",function(data){
+ajax("ajax/actions/countries.php", function(data){
     var html = "";
     for (var i in data){
         var country = data[i];
@@ -10,15 +10,6 @@ ajax("ajax/actions/countries.php",function(data){
     }
 
     $(".country-selector").innerHTML = html;
-});
-
-$("#login-button").addEventListener("click", function (event) {
-    event.preventDefault();
-    PostAjax("ajax/actions/login.php", "username=" + $("#username").value + "&password=" + $("#password").value, function (data) {
-        if (data.error === undefined) {
-            window.location = "?p=home";
-        }
-    });
 });
 
 $("#register-button").addEventListener("click", function (event) {
