3dfb52befa677f035d59b473270ad34ab5ec9930
Baran Skistad <baranjskistad@gmail.com>
2016-10-12 21:22:02 -0500

    Updated site key.

--- views/pages/login.php
+++ views/pages/login.php
@@ -3,7 +3,7 @@
     Username: <input type="text" placeholder='Your Username' name="username" id="username"><br>
     Password:
     <input type="password" placeholder='Your Password' name="password" id="password"><br>
-    <div class="g-recaptcha" data-sitekey="6LcOgSUTAAAAAECwyZJthl4OgPTDygWKt2YuEwis"></div><br>
+    <div class="g-recaptcha" data-sitekey="6Ld3HwkUAAAAANjidQgzLOMgGQz7udFshwdM8aBX"></div><br>
     <button type="submit" id="login-button">Login</button>
 </form>
 </div>
